e7f2386845df1460abad7fd6af0aae1854f46031
yakoubi rayen <rayen08yako@gmail.com>
2024-05-28 19:37:52 +0100

    Add .gitignore file to exclude unnecessary files

---
+++ client/src/assets/stickersUrl.js
@@ -0,0 +1,8 @@
+export const stickersUrl = [
+  {id : 1, url : 'src/assets/D_ragon.jpg'},
+  {id : 2, url : '../assets/Dragula.jpg'},
+  {id : 3, url : 'src/assets/Dragyran.jpg'},
+  {id : 4, url : 'src/assets/kill.jpg'},
+  {id : 5, url : 'src/assets/KiLLuLa.jpg'},
+  {id : 5, url : 'src/assets/Dragysynos.jpg'},
+]
--- client/src/pages/Stickers/Stickers.jsx
+++ client/src/pages/Stickers/Stickers.jsx
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {stickersUrl} from '../../assets/stickersUrl';
 
 const App = () => {
   const [stickers, setStickers] = useState([]);
-
+  
   useEffect(() => {
-    axios.get('https:/localhost:4000/api/stickers')
+    console.log(stickersUrl)
+    axios.get('http://localhost:4000/api/stickers/allStickers')
       .then(response => setStickers(response.data))
       .catch(error => console.error(error));
-  }, []);
+  }, [stickersUrl]);
 
   return (
     <div>
-      <h1>Stickers Shop</h1>
+      <h1 className='text-4xl p-20'>Stickers Shop</h1>
       <ul>
         {stickers.map(sticker => (
           <li key={sticker._id}>
             <h2>{sticker.name}</h2>
             <p>{sticker.description}</p>
             <p>{sticker.price}</p>
-            <img src={sticker.imageUrl} alt={sticker.name} width="100" />
+            <img src={stickersUrl[0].url} alt={sticker.name} width="100" />
+            {/* <img src={sticker.imageUrl} alt={sticker.name} width="100" /> */}
           </li>
         ))}
       </ul>
